7023522f7026056f77f1747edad2df58c2dd14a7
Brun-00 <bruno.lm.2422@gmail.com>
2026-09-16 15:38:02 -0300

    GameManager correction

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -78,7 +78,6 @@ public class GameManager : Singleton<GameManager>
     {
         stateMachine = new StateMachine<GameStates>();
         stateMachine.Init();
-        stateMachine.RegisterStates(GameStates.INTRO, new GMStateIntro());
         stateMachine.RegisterStates(GameStates.GAMEPLAY, new StateBase());
         stateMachine.RegisterStates(GameStates.PAUSE, new StateBase());
         stateMachine.RegisterStates(GameStates.WIN, new StateBase());
